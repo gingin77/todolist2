8b42562222e97b131bb5ed6ba22f1f90fe34f573
ginniehench <ginniehench@gmail.com>
2017-08-17 17:01:23 -0400

    the marked as compkete button function is not working

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var bodyParser = require('body-parser');
 
 let todos = ["Research pet insurance", "Take dog to vet"]
 let new_todos = "";
+let completed = ["paid tuition for TIY"]
+
+
+
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
@@ -17,14 +21,26 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get("/todo2/", function (req, res) {
-  res.render('todo2', { todos: todos });
+  res.render('todo2', { todos: todos, completed: completed });
 });
 
 
 app.post("/todo2/", function (req, res) {
   todos.push(req.body.new_todos);
-  // res.send('todo2', { todos: new_todos });
-  console.log(todos)
+  // completed.push(req.body.new_todos)
+  // let popped = todos.pop();
+  // completed.push(popped);
+  // console.log(popped);
+  console.log(todos);
+  res.redirect('/todo2/');
+})
+
+app.post("/todo2/", function (req, res) {
+  completed.push(req.body.marked_as_complete)
+  // let popped = todos.pop();
+  // completed.push(popped);
+  // console.log(popped);
+  console.log(completed);
   res.redirect('/todo2/');
 })
 
