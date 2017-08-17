5739398b31e2c3e67b12504d61f77ef2c069f86e
ginniehench <ginniehench@gmail.com>
2017-08-17 15:29:26 -0400

    I'm able to collect form data and console.log it. However, I'm not yet able to render it back to the page

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
+
+let todos = ["Research pet insurance", "Take dog to vet"]
+let new_todos = "";
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(expressValidator);
+
+
+
+app.get("/todo2/", function (req, res) {
+  res.render('todo2', { todos: new_todos });
+});
+
+
+app.post("/todo2/", function (req, res) {
+  todos.push(req.body.new_todos);
+  console.log(todos)
+  res.redirect('/todo2/');
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
